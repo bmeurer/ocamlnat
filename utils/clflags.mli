a1e120e2d67760d0fc3405c84b2542a28e842103
Benedikt Meurer <benedikt.meurer@googlemail.com>
2011-11-01 15:41:16 +0100

    [utils] Add missing bits to the Clflags interface for compatibility.

--- utils/clflags.mli
+++ utils/clflags.mli
@@ -12,11 +12,21 @@
 
 (* Command-line parameters *)
 
+val objfiles : string list ref
+val ccobjs : string list ref
+val dllibs : string list ref
+val compile_only : bool ref
+val output_name : string option ref
 val include_dirs : string list ref
 val no_std_include : bool ref
 val print_types : bool ref
+val make_archive : bool ref
 val debug : bool ref
 val fast : bool ref
+val link_everything : bool ref
+val custom_runtime : bool ref
+val output_c_object : bool ref
+val ccopts : string list ref
 val classic : bool ref
 val nopervasives : bool ref
 val preprocessor : string option ref
@@ -27,16 +37,24 @@ val noassert : bool ref
 val verbose : bool ref
 val noprompt : bool ref
 val init_file : string option ref
+val use_prims : string ref
+val use_runtime : string ref
 val principal : bool ref
 val recursive_types : bool ref
 val strict_sequence : bool ref
 val applicative_functors : bool ref
+val make_runtime : bool ref
 val gprofile : bool ref
 val c_compiler : string option ref
+val no_auto_link : bool ref
+val dllpaths : string list ref
+val make_package : bool ref
+val for_package : string option ref
 val dump_parsetree : bool ref
 val dump_rawlambda : bool ref
 val dump_lambda : bool ref
 val dump_instr : bool ref
+val keep_asm_file : bool ref
 val optimize_for_speed : bool ref
 val register_allocator : string ref
 val dump_cmm : bool ref
@@ -51,6 +69,7 @@ val dump_regalloc : bool ref
 val dump_reload : bool ref
 val dump_scheduling : bool ref
 val dump_linear : bool ref
+val keep_startup_file : bool ref
 val dump_combine : bool ref
 val native_code : bool ref
 val inline_threshold : int ref
