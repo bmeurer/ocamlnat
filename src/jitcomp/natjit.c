a22f5a0d6c5cf169bad6773fb633e3872d20dde1
Benedikt Meurer <benedikt.meurer@googlemail.com>
2011-11-13 23:19:36 +0100

    [jit] Add preliminary support for unaligned memory access on non-x86 arches.

--- src/jitcomp/natjit.c
+++ src/jitcomp/natjit.c
@@ -105,11 +105,6 @@ static void *mmap_p(mlsize_t size, int prot){
 
 #include <windows.h>
 
-#ifdef _MSC_VER
-typedef signed __int16   int16_t;
-typedef unsigned __int16 uint16_t;
-#endif
-
 #define PROT_RW  PAGE_READWRITE
 #define PROT_RWX PAGE_EXECUTE_READWRITE
 
@@ -138,34 +133,122 @@ static void mprotect(void *addr, mlsize_t size, int prot){
 
 value camlnat_jit_getint16(value str, value ofs)
 {
-  return Val_long(*(const int16_t *)(String_val(str) + Long_val(ofs)));
+  unsigned char *p = &Byte_u(str, Long_val(ofs));
+#if defined(ARCH_BIG_ENDIAN)
+  unsigned x = (unsigned)p[0] << 8
+             | (unsigned)p[1];
+#else
+  unsigned x = (unsigned)p[0]
+             | (unsigned)p[1] << 8;
+#endif
+  return Val_long(x);
 }
 
 value camlnat_jit_getint32(value str, value ofs)
 {
-  return caml_copy_int32(*(const int32 *)(String_val(str) + Long_val(ofs)));
+  unsigned char *p = &Byte_u(str, Long_val(ofs));
+#if defined(TARGET_amd64) || defined(TARGET_i386)
+  uint32 x = *(const uint32 *)p;
+#elif defined(ARCH_BIG_ENDIAN)
+  uint32 x = (uint32)p[0] << 24
+           | (uint32)p[1] << 16
+           | (uint32)p[2] << 8
+           | (uint32)p[3];
+#else
+  uint32 x = (uint32)p[0]
+           | (uint32)p[1] << 8
+           | (uint32)p[2] << 16
+           | (uint32)p[3] << 24;
+#endif
+  return caml_copy_int32(x);
 }
 
 value camlnat_jit_getint64(value str, value ofs)
 {
-  return caml_copy_int64(*(const int64 *)(String_val(str) + Long_val(ofs)));
+  unsigned char *p = &Byte_u(str, Long_val(ofs));
+#if defined(TARGET_amd64) || defined(TARGET_i386)
+  uint64 x = *(const uint64 *)p;
+#elif defined(ARCH_BIG_ENDIAN)
+  uint64 x = (uint64)p[0] << 56
+           | (uint64)p[1] << 48
+           | (uint64)p[2] << 40
+           | (uint64)p[3] << 32
+           | (uint64)p[4] << 24
+           | (uint64)p[5] << 16
+           | (uint64)p[6] << 8
+           | (uint64)p[7];
+#else
+  uint64 x = (uint64)p[0]
+           | (uint64)p[1] << 8
+           | (uint64)p[2] << 16
+           | (uint64)p[3] << 24
+           | (uint64)p[4] << 32
+           | (uint64)p[5] << 40
+           | (uint64)p[6] << 48
+           | (uint64)p[7] << 56;
+#endif
+  return caml_copy_int64(x);
 }
 
 value camlnat_jit_putint16(value str, value ofs, value val)
 {
-  *((int16_t *)(String_val(str) + Long_val(ofs))) = Long_val(val);
+  unsigned char *p = &Byte_u(str, Long_val(ofs));
+  unsigned x = Long_val(val);
+#if defined(ARCH_BIG_ENDIAN)
+  *p++ = x >> 8;
+  *p++ = x;
+#else
+  *p++ = x;
+  *p++ = x >> 8;
+#endif
   return Val_unit;
 }
 
 value camlnat_jit_putint32(value str, value ofs, value val)
 {
-  *((int32 *)(String_val(str) + Long_val(ofs))) = Int32_val(val);
+  unsigned char *p = &Byte_u(str, Long_val(ofs));
+  uint32 x = Int32_val(val);
+#if defined(TARGET_amd64) || defined(TARGET_i386)
+  *(uint32 *)p = x;
+#elif defined(ARCH_BIG_ENDIAN)
+  *p++ = x >> 24;
+  *p++ = x >> 16;
+  *p++ = x >> 8;
+  *p++ = x;
+#else
+  *p++ = x;
+  *p++ = x >> 8;
+  *p++ = x >> 16;
+  *p++ = x >> 24;
+#endif
   return Val_unit;
 }
 
 value camlnat_jit_putint64(value str, value ofs, value val)
 {
-  *((int64 *)(String_val(str) + Long_val(ofs))) = Int64_val(val);
+  unsigned char *p = &Byte_u(str, Long_val(ofs));
+  uint64 x = Int64_val(val);
+#if defined(TARGET_amd64) || defined(TARGET_i386)
+  *(uint64 *)p = x;
+#elif defined(ARCH_BIG_ENDIAN)
+  *p++ = x >> 56;
+  *p++ = x >> 48;
+  *p++ = x >> 40;
+  *p++ = x >> 32;
+  *p++ = x >> 24;
+  *p++ = x >> 16;
+  *p++ = x >> 8;
+  *p++ = x;
+#else
+  *p++ = x;
+  *p++ = x >> 8;
+  *p++ = x >> 16;
+  *p++ = x >> 24;
+  *p++ = x >> 32;
+  *p++ = x >> 40;
+  *p++ = x >> 48;
+  *p++ = x >> 56;
+#endif
   return Val_unit;
 }
 
